a72e1a3f42e1b6f17c20b7ee747a4277245054cb
Paula Speciale <paulaspeciale@gmail.com>
2022-06-18 13:03:36 -0300

    probando

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -204,10 +204,11 @@ function combinarNombres(nombre, apellido) {
 }
 
 function obtenerSaludo(nombre) {
-  // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
+  // Toma la string "nombre" y concatena otras string en la cadena para que
+  // tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  return ('Hola '+nombre);
+  return ('Hola '+nombre+"!");
   
 }
 
